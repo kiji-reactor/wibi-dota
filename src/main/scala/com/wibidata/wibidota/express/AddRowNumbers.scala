4ef605c50dcbdd7ae0c91fa5a2b4a366eea522e5
Chris Clark <chris@wibidata.com>
2013-08-12 12:14:00 -0700

    Added AddRowNumber

---
+++ src/main/scala/com/wibidata/wibidota/express/AddRowNumbers.scala
@@ -0,0 +1,12 @@
+package com.wibidata.wibidota.express
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chris
+ * Date: 8/12/13
+ * Time: 11:09 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class AddRowNumbers {
+
+}
