bea9b19d2d83376141c9eac4fa74ec6b6d04090d
Chris Clark <chris@wibidata.com>
2013-08-17 15:09:23 -0700

    Expanded to included lobby_type and hero names

--- src/main/python/dota_values.py
+++ src/main/python/dota_values.py
@@ -5,10 +5,39 @@ Currently this is being built as needed so it does not contain a complete
 mapping.
 """
 
+import os
+import json
+
+HERO_NAMES_MAP = None
+
+HOME = os.environ.get("WIBIDOTA")
 
 GAME_MODES = ["GAME_MODE_ZERO", "ALL_PICK", "CAPTAINS_MODE", "RANDOM_DRAFT",
     "SINGLE_DRAFT", "ALL_RANDOM", "GAME_MODE_SIX", "THE_DIRETIDE",
     "REVERSE_CAPTAINS_MODE", "GREEVILING", "TUTORIAL", "MID_ONLY",
     "LEAST_PLAYED", "NEW_PLAYER_POOL", "COMPENDIUM"]
+
+LOBBY_TYPES = ["INVALID", "PUBLIC_MATCHMAKING", "PRACTICE", "TOURNAMENT", 
+               "TUTORIAL", "CO_OP_WITH_BOTS", "TEAM_MATCH", "SOLO_QUEUE"]
+
 def get_game_mode(gm_id):
   return GAME_MODES[gm_id]
+
+def get_lobby_type(lobby_type_id):
+  return LOBBY_TYPES[lobby_type_id + 1]
+
+def build_map_from_json(filename, container, id_field, name_field):
+  m = {}
+  json_values = json.load(open(filename))[container]
+  for val in json_values:
+    m[int(json.dumps(val[id_field]))] = json.dumps(val[name_field]).strip("\"")
+  return m
+  
+
+def get_hero_name(hero_id, allow_unknown = False):
+  global HERO_NAMES_MAP
+  if(HERO_NAMES_MAP == None):
+    HERO_NAMES_MAP = build_map_from_json(HOME + "/src/main/resources/com/wibidata/wibidota/heroes.json", "heroes", "id", "localized_name")
+  if(allow_unknown and hero_id not in HERO_NAMES_MAP):
+    return "UNKNOWN"
+  return HERO_NAMES_MAP[hero_id]
