1507b0cdb975c486d3546fe2cb120427f902672e
Chris Clark <chris@wibidata.com>
2013-08-17 15:10:14 -0700

    Updated, now translates some column names into human readable form

--- src/main/python/parse_histogram_counts.py
+++ src/main/python/parse_histogram_counts.py
@@ -6,6 +6,7 @@ stacked area plot, but it does not look great. I have had better luck with open'
 stacked area plot. 
 """
 
+import util
 from os import listdir
 from os.path import isfile
 import sys
@@ -15,9 +16,12 @@ from matplotlib import cm
 from collections import defaultdict
 from operator import itemgetter
 import datetime
+from dota_values import get_game_mode
+from dota_values import get_lobby_type
+
 
 DATE_FORMAT = '%m-%d-%Y'
-INTERVAL = 60*60*24*7 # One Week
+INTERVAL = 7 * 60 * 60 * 24
 TYPES = [str, str, int, int, int]
 FIELD = 0
 VALUE = 1
@@ -27,8 +31,9 @@ COUNT = 4
 
 # TODO should rename kv_counts to something else 
 
-def kv_counts_from_file(file_name, kv_counts):
-  for line in open(file_name):
+def kv_counts_from_folder(file_name):
+  kv_counts = defaultdict(lambda : defaultdict(list))
+  for line in util.get_lines(folder_name):
     counts = []
     for i, part in enumerate(line.rstrip().split(",")):
       counts.append(TYPES[i](part))
@@ -37,21 +42,16 @@ def kv_counts_from_file(file_name, kv_counts):
       # Valve's end, currently we just ignore those values
       print("FOUND A ZERO VALUE, (line: " + line.rstrip() + " continuing")
       continue
-    kv_counts[counts[FIELD]][counts[VALUE]].append((counts[START], counts[COUNT]))
-
-def kv_counts_from_folder(folder_name):
-  kv_counts = defaultdict(lambda : defaultdict(list))
-  for file_name in listdir(folder_name):
-    file_name = folder_name + "/" + file_name
-    if(isfile(file_name) and not file_name.startswith("_")):
-      kv_counts_from_file(file_name, kv_counts)
+    kv_counts[counts[FIELD]][counts[VALUE]].append((counts[START] / 1000, counts[COUNT]))
   return kv_counts
 
-def print_csv(kv_counts):
+def print_csv(field, kv_counts):
+  print("Printing data points for: " + field)
   for kv_str, (x_lst, y_lst) in kv_counts.iteritems():        
     x_values = x_lst
     print(kv_str + "," + ",".join(map(str, y_lst)))
   print("X," + ",".join(map(lambda x : (datetime.datetime.fromtimestamp(x).strftime(DATE_FORMAT)), x_values)))
+
 
 def graph(kv_counts):
   for x_lst, y_lst in kv_counts.itervalues():
@@ -87,10 +87,25 @@ def to_data_points(value_counts):
       all_data_points[value][1].append(data_points[(x - min_x) / INTERVAL])
   return all_data_points
 
+def translate_values(kv_counts):
+  for field, value_counts in kv_counts.items():
+    if(field == "game_mode"):
+      new_dict = {}
+      for value, counts in value_counts.iteritems():
+        new_dict[get_game_mode(int(value))] = counts 
+      kv_counts[field] = new_dict
+    elif(field == "lobby_type"):
+      new_dict = {}
+      for value, counts in value_counts.iteritems():
+        new_dict[get_lobby_type(int(value))] = counts 
+      kv_counts[field] = new_dict
+
 if __name__ == "__main__":
   folder_name = sys.argv[1]
-  kv_counts = kv_counts_from_folder(folder_name)    
+  kv_counts = kv_counts_from_folder(folder_name)       
+  translate_values(kv_counts)
   for field, value_counts in kv_counts.iteritems():
     data_points = to_data_points(value_counts)
-    graph(data_points)
+    print_csv(field, data_points)
+#    graph(data_points)
 
