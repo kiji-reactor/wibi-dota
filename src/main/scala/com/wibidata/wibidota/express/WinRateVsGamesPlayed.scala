e805a66c03257e43433ad513d65c228b06369d32
Chris Clark <chris@wibidata.com>
2013-08-19 23:57:35 -0700

    Add documentation to winRateVsGamesPlayed

--- src/main/scala/com/wibidata/wibidota/express/WinRateVsGamesPlayed.scala
+++ src/main/scala/com/wibidata/wibidota/express/WinRateVsGamesPlayed.scala
@@ -6,6 +6,14 @@ import com.wibidata.wibidota.express.DefaultResourceLocations._
 import org.kiji.express.{AvroRecord, KijiSlice}
 import com.wibidata.wibidota.DotaValues
 
+/**
+ * Calculates the win rate players has across a n game sliding window,
+ * then find the size, mean, and standard deviation of the win rates
+ * players have for their first window, second window, and so.
+ *
+ * @param args, arguements including
+ * --window_size, how large the window should be
+ */
 class WinRateVsGamesPlayed(args: Args) extends KijiJob(args) {
 
   override def config(implicit mode: Mode): Map[AnyRef, AnyRef] = super.config(mode) ++ Map(
@@ -19,6 +27,7 @@ class WinRateVsGamesPlayed(args: Args) extends KijiJob(args) {
       Column("match_derived_data:real_match", all) -> 'realMatch
     )
   )
+    // Map players to a (win_rate, game_number) pairs for each real_match
     .flatMapTo(('player, 'radiantWin, 'realMatch) -> ('win, 'game_number)){
     f : (KijiSlice[AvroRecord], KijiSlice[Boolean], KijiSlice[Double]) =>
       val rms = f._3.cells.filter(x => x.datum >= 2.0).map(x => x.version).toSet
@@ -33,12 +42,12 @@ class WinRateVsGamesPlayed(args: Args) extends KijiJob(args) {
         } else {
           0.0
         }
-//      })
       }).sliding(args("window_size").toInt).map(x => {
         game += 1;
         (x.sum / x.size, game)
       }).toIterable
   }
+    // Group by game_number and calculate the stats we want
   .groupBy('game_number){_.sizeAveStdev('win-> ('size, 'av, 'std))}
     .write(Csv(args("output"), writeHeader = true))
 }
