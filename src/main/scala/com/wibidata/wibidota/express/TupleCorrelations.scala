438ae33ea8d2e8eb30a28adc405003d6fbd14c16
Chris Clark <chris@wibidata.com>
2013-08-22 15:04:56 -0700

    Added TupleCorrelations

--- src/main/scala/com/wibidata/wibidota/express/TupleCorrelations.scala
+++ src/main/scala/com/wibidata/wibidota/express/TupleCorrelations.scala
@@ -1,5 +1,8 @@
 package com.wibidata.wibidota.express
 
+import com.twitter.scalding.{Csv, Args}
+import org.kiji.express.flow.KijiJob
+
 /**
  * Created with IntelliJ IDEA.
  * User: chris
@@ -7,6 +10,10 @@ package com.wibidata.wibidota.express
  * Time: 3:30 PM
  * To change this template use File | Settings | File Templates.
  */
-class TupleCorrelations {
+class TupleCorrelations(args: Args) extends KijiJob(args) with CalcCorrelations {
+
+  val input = Csv(args("input"), ",", ('vec, 'index, 'val))
+
+  nonZeroCorrelations(input).write(Csv(args("output"), writeHeader = true))
 
 }
