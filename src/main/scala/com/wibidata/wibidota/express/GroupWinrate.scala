77bf510693f623a543670bc6b2b5ccb8f61366c3
Chris Clark <chris@wibidata.com>
2013-08-23 14:08:27 -0700

    Cleaned upped and bug fix to GroupWinrate

--- src/main/scala/com/wibidata/wibidota/express/GroupWinrate.scala
+++ src/main/scala/com/wibidata/wibidota/express/GroupWinrate.scala
@@ -9,33 +9,36 @@ import com.wibidata.wibidota.DotaValues
 /**
  * Groups the winrates of games by the the valueS in an arbitrary column of
  * match_derived_data in dota_players. Returns the standard deviation, average,
- * and counts.
+ * and count.
  *
- * this
  * @param args
  */
 class GroupWinrate(args: Args) extends KijiJob(args) {
   override def config(implicit mode: Mode): Map[AnyRef, AnyRef] = super.config(mode) ++
     Map (
-      "hbase.client.scanner.caching" -> "100"
+      "hbase.client.scanner.caching" -> "100",
+      "mapred.task.profile" -> "true",
+  "mapred.task.profile.reduces" -> "0",
+  "mapred.task.profile.map" -> "0-2",
+  "mapred.task.profile.params" -> "-agentlib:hprof=cpu=samples,heap=sites,depth=9,force=n,thread=y,verbose=n,file=%s"
+
     )
 
   KijiInput(args.getOrElse("table", PlayerTable))(
     Map (
-      Column("match_dervied_data" + args("column"), all) -> 'values,
+      Column("match_derived_data:" + args("column"), all) -> 'values,
       Column("data:radiant_win", all) -> 'rWin,
       Column("data:player", all) -> 'player
     )
-  ).flatMapTo(('values, 'rWin, 'player) -> ('streak, 'win)){
+  )
+    .flatMapTo(('values, 'rWin, 'player) -> ('values, 'win)){
     x : (KijiSlice[Double], KijiSlice[Boolean], KijiSlice[AvroRecord]) =>
-      System.out.println(x._1.cells.size)
-      System.out.println(x._2.cells.size)
-      System.out.println(x._3.cells.size)
       val useMatches = x._1.cells.map(a => a.version).toSet
-      (x._1.cells, x._2.cells.filter(x => useMatches.contains(x.version)), x._3.cells.filter(x => useMatches.contains(x.version))).zipped.toIterable.map({c =>
+      val m = (x._1.cells, x._2.cells.filter(x => useMatches.contains(x.version)), x._3.cells.filter(x => useMatches.contains(x.version))).zipped.toIterable.map({c =>
         val win = c._2.datum == DotaValues.radiantPlayer(c._3.datum("player_slot").asInt())
         (c._1 .datum, if(win) 1.0 else 0.0)
       }).toIterable
-  }.groupBy('streak){gb => gb.sizeAveStdev('win -> ('size, 'av, 'std))}
+      m
+  }.groupBy('values){gb => gb.sizeAveStdev('win -> ('size, 'av, 'std))}
     .write(Csv(args("output"), writeHeader = true))
 }
