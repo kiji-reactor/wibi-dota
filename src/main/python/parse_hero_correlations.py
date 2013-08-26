f068da34f17af080397663e02bdede806c16f703
Chris Clark <chris@wibidata.com>
2013-08-26 15:17:18 -0700

    Added the ability to interact with an additional size fields

--- src/main/python/parse_hero_correlations.py
+++ src/main/python/parse_hero_correlations.py
@@ -14,13 +14,21 @@ def get_corr(folder_name):
   for line in util.get_lines(folder_name):
     parts = line.rstrip().split(",")
     if(parts[2] != "NaN"):
-      correlations.append((dota_values.get_hero_name(int(parts[0]), True),
-                           dota_values.get_hero_name(int(parts[1]), True),
-                           float(parts[2])))
+      if(len(parts) == 4):
+        correlations.append((dota_values.get_hero_name(int(parts[0]), True),
+                             dota_values.get_hero_name(int(parts[1]), True),
+                             float(parts[2]), int(parts[3])))
+      else:
+        correlations.append((dota_values.get_hero_name(int(parts[0]), True),
+                             dota_values.get_hero_name(int(parts[1]), True),
+                             float(parts[2])))
   return correlations
 
 if __name__ == "__main__":
   folder_name = sys.argv[1]  
   corrs = get_corr(folder_name)
+  # Optionally use can filter by size if a size field is present
+  if(len(corrs[0]) == 4 and len(sys.argv) == 2):
+    corrs = filter(lambda x : x[3] > int(sys.argv[2], corrs))
   corrs.sort(key = lambda x : abs(x[2]))
   print("\n".join(map(lambda x : ",".join(map(str, x)), corrs)))
